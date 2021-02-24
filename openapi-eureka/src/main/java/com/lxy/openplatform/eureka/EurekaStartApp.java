3e2ef11ad75cf70b07636d860941255d85694f32
L-960 <960609477@qq.com>
2021-02-25 00:32:18 +0800

    eureka模块v1

---
+++ openapi-eureka/src/main/java/com/lxy/openplatform/eureka/EurekaStartApp.java
@@ -0,0 +1,15 @@
+package com.lxy.openplatform.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaStartApp {
+
+    public static void main (String[] args){
+        SpringApplication.run(EurekaStartApp.class,args);
+    }
+}
+
---
+++ openapi-eureka/src/main/java/com/lxy/openplatform/eureka/config/EurekaConfig.java
@@ -0,0 +1,24 @@
+package com.lxy.openplatform.eureka.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Author Lvxy
+ * @Date 2021/2/25 0:15
+ * @Version 1.0
+ * @Desc 我们的eureka的配置,防止无法注册服务
+ */
+@EnableWebSecurity
+public class EurekaConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()//禁用掉csrf防攻击,防止我们的服务无法注册
+                .authorizeRequests()//需要认证所有的请求
+                .mvcMatchers("/eureka/**").permitAll()//注册地址放行
+                .mvcMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated().and().httpBasic();
+    }
+}
